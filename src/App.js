4aec9bafc9e22bba79a962c5e3fc25bb6c9d8b27
Caden <caden@dowd.org>
2023-12-25 20:40:31 -0800

    trivia guy v1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,117 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Card from './Card';
+
+const categories = [
+  {
+    category: "General",
+    color: "lightgreen",
+  },
+  {
+    category: "Entertainment",
+    color: "lightpink",
+  },
+  {
+    category: "Sports",
+    color: "lightblue",
+  },
+  {
+    category: "Business",
+    color: "lavendar",
+  },
+  {
+    category: "Technology",
+    color: "lightgray",
+  },
+];
 
 function App() {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const handleCall =  async () => {
+    const fetchArticles = async () => {
+      try {
+        setLoading(true);
+        // abc news
+        // const response = await fetch('https://newsapi.org/v2/top-headlines?pageSize=21&sources=abc-news&language=en&apiKey=ff6c70e621e54ff19b943a356a78973d');
+        // all business sources 
+        // const response = await fetch('https://newsapi.org/v2/top-headlines/sources?category=business&apiKey=ff6c70e621e54ff19b943a356a78973d');
+
+        const apiKey = process.env.REACT_APP_NEWS_API_KEY;
+        const responses = await Promise.all([
+          fetch(`https://newsapi.org/v2/top-headlines?category=general&language=en&apiKey=${apiKey}`),
+          // fetch(`https://newsapi.org/v2/top-headlines?category=entertainment&language=en&apiKey=${apiKey}`),
+          // fetch(`https://newsapi.org/v2/top-headlines?category=sports&language=en&apiKey=${apiKey}`),
+          // fetch(`https://newsapi.org/v2/top-headlines?category=business&language=en&apiKey=${apiKey}`),
+          // fetch(`https://newsapi.org/v2/top-headlines?category=technology&language=en&apiKey=${apiKey}`),
+
+          // fetch('https://newsapi.org/v2/top-headlines/sources?country=us&language=en&apiKey=ff6c70e621e54ff19b943a356a78973d'),
+        ]);
+
+        const data = await Promise.all(responses.map(response => response.json()));
+        console.log(data);
+        const zipped = data.map((article, i) => {
+          return {
+            "articles": article.articles,
+            "category": categories[i].category,
+            "color": categories[i].color,
+          }
+        })
+        console.log(zipped);
+        setData(zipped);
+        // sf headlines === try nbc northern california? 
+        // const resp = await fetch('https://newsapi.org/v2/top-headlines?q=san%20francisco&q=bay%20area&pageSize=30&language=en&apiKey=ff6c70e621e54ff19b943a356a78973d');
+        // const sf = await resp.json();
+        // console.log(sf)
+        // setSf(sf.articles
+
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    await fetchArticles();
+  };
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* TODO nav bar with our info */}
+      <h2>Caden's trivia helper v1</h2>
+      <p>click below for recent headlines</p>
+      <button
+        style={{marginTop: '10px'}}
+        onClick={() => {handleCall()}}
+      >
+        click me
+      </button>
+      {
+        error &&
+        <div>there was an error. try again tomorrow.</div>
+      }
+      {
+        data?.status === 'error' ? 
+          <div>error please try again tomorrow.</div>
+         : data ?
+          data.map((item, i) => {
+            return (<Card
+              key={i}
+              articles={item.articles}
+              category={item.category}
+              color={item.color}
+            />)
+          }) :
+        <></>
+      } 
     </div>
   );
 }
 
 export default App;
+
+
+// "message": "Your API key is invalid or incorrect. Check your key, or go to https://newsapi.org to create a free API key."
---
+++ src/Card.js
@@ -0,0 +1,69 @@
+import React, {useState, useMemo} from "react";
+
+function shuffleArray(originalArray) {
+    // Creating a copy of the original array
+    const array = [...originalArray];
+    
+    // Fisher-Yates Shuffle Algorithm
+    for (let i = array.length - 1; i > 0; i--) {
+        // Generate a random index
+        const j = Math.floor(Math.random() * (i + 1));
+
+        // Swap elements at indices i and j
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+
+    return array;
+}
+
+const Card = (props) => {
+    const {
+        articles,
+        category,
+        color,
+    } = props;
+
+    const [loading, setLoading] = useState(false);
+    const [shuffledArticles, setShuffledArticles] = useState(shuffleArray(articles));
+
+    const handleShufle = () => {
+        setLoading(true);
+        const shuffleAgain = shuffleArray(shuffledArticles);
+        setShuffledArticles(shuffleAgain);
+        setLoading(false);
+    }
+    
+    return (
+        <div 
+          style={{
+            backgroundColor: color,
+            padding: '2rem',
+            margin: '1rem 2rem',
+            borderRadius: '10px'
+          }}
+        >
+            <div style={{display: 'flex', justifyContent: 'space-between'}}>
+                <h3>{category}</h3>
+                <button onClick={() => {handleShufle()}}>refresh</button>
+            </div>
+            <ol>
+            {
+                shuffledArticles.slice(0, 5).map((article, i) => {
+                    const title = article.title;
+                    const short = title.substring(0, title.lastIndexOf('-'));
+                    return (
+                        <li key={`${category}-i`}>
+                            <div style={{marginTop: i !== 0 ? '30px': '0px'}}>
+                                <h3>{short}</h3>
+                                <p>{article.description} - {article.source.name}</p>
+                                {/* <p>{article.content}</p> */}
+                            </div>
+                        </li>
+                    )})
+            }
+            </ol>
+        </div>
+    )
+}
+
+export default Card;
